5c6a3d712eee3c0b100d8a92160a3f9986ce06e2
Lara Hofmann <lara@Laras-MacBook-Pro.local>
2022-05-31 19:10:48 +0200

    add barrier payoff

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -1,3 +1,4 @@
 from .EuropeanCall import EuropeanCall
 from .MertonJumpCall import MertonJumpCall
 from .MertonJumpPut import MertonJumpPut
+from .payoff_barrier import payoff_barrier
---
+++ instruments/payoff_barrier.py
@@ -0,0 +1,13 @@
+def payoff_barrier(x,K,b,putorcall):
+    num_paths, time_steps = np.shape(x)
+    payoff = np.zeros(num_paths)
+    for i in range(num_paths):
+        x_i = x[i,:]
+    if np.max(x_i) < b:
+        if putorcall == "call":
+            payoff[i] = -np.maximum(x_i[-1] - K, 0.0)
+        elif putorcall == "put":
+            payoff[i] = -np.maximum(K - x_i[-1], 0.0)
+    else:
+        payoff[i] = 0
+    return payoff
